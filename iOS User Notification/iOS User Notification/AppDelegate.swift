0daa5c3572c8e1198aaa97f26631c4b45a26eeb5
buddhdev-jay <jay.b@simformsolutions.com>
2022-07-20 12:44:44 +0530

    Base Setup and Firebase Integration

---
+++ iOS User Notification/iOS User Notification/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  iOS User Notification
+//
+//  Created by Jay Buddhdev on 20/07/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+       // Will be Implemeted Later
+    }
+
+}
---
+++ iOS User Notification/iOS User Notification/SceneDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  SceneDelegate.swift
+//  iOS User Notification
+//
+//  Created by Jay Buddhdev on 20/07/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Will be Implemeted Later
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Will be Implemeted Later
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Will be Implemeted Later
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Will be Implemeted Later
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Will be Implemeted Later
+    }
+
+}
